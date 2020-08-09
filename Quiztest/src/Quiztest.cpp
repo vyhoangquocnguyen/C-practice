5be3c340cb20101e8a555d7ad1e34873682dd7cb
Vy Nguyễn <36269757+vynguyen3112@users.noreply.github.com>
2020-08-09 01:18:35 -0400

    Add files via upload

--- Quiztest/src/Quiztest.cpp
+++ Quiztest/src/Quiztest.cpp
@@ -9,19 +9,30 @@
 #include <iostream>
 #include <vector>
 #include <stdexcept>
+#include <algorithm>
 
 using namespace std;
 
 struct student_struct
 {
 	string name;
-	int score;
+	int score ;
 };
 typedef student_struct stn_t;
+
+istream& read(istream &is, stn_t &indv)
+{
+	is >> indv.name >>  indv.score;
+	return is;
+}
 
 bool result_sorter(stn_t a, stn_t b)
 {
 	if(a.score > b.score)
+		return true;
+	else
+		return false;
+	if(a.name < b.name)
 		return true;
 	else
 		return false;
@@ -61,6 +72,8 @@ int quiz2(int score)
 		throw logic_error("Wrong answer");
 	return score;
 }
+/*Third Question*/
+
 int quiz3(int score)
 {
 	int ans3;
@@ -75,7 +88,8 @@ int quiz3(int score)
 		throw logic_error("Wrong answer");
 	return score;
 }
-void test()
+
+stn_t test(stn_t a)
 {
 	int score = 0;
 	try {
@@ -101,11 +115,34 @@ void test()
 	{
 		cout << str1.what() << endl;
 	}
+
 	cout << "Your score: " << score << endl;
+	a.score = score;
+	return a;
 }
 
-int main() {
-
+int main()
+{
+	/*Variables declearation*/
+	vector<stn_t> victims;
+	stn_t indv;
+	cout << "Enter your name and take the test" << endl;
+	cout << "Press Ctrl + D when done" << endl;
+	while (cin >> indv.name)
+	{
+		indv = test(indv);
+		victims.push_back(indv);
+		cout << "Student Name: "<<indv.name << "--- Score: " <<indv.score << endl;
+	}
+	/*
+	 * Sorting and Displaying the result.
+	 * */
+	sort(victims.begin(),victims.end(),result_sorter);
+	cout << " The Results are: " << endl;
+	for(vector<stn_t>::size_type ii = 0; ii !=victims.size(); ++ii)
+		{
+		cout <<"Student: " << victims[ii].name <<"---Score: " << victims[ii].score << endl;
+		}
 
 	return 0;
 }
