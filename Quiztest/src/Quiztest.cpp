fb3c7858f8ffafc4b4bee2b2495dab2160b06699
Vy Nguyễn <36269757+vynguyen3112@users.noreply.github.com>
2020-08-09 22:29:23 -0400

    Quiztest3

--- Quiztest/src/Quiztest.cpp
+++ Quiztest/src/Quiztest.cpp
@@ -13,36 +13,31 @@
 
 using namespace std;
 
-struct student_struct
-{
+struct student_struct {
 	string name;
-	int score ;
+	int score;
 };
 typedef student_struct stn_t;
 
-istream& read(istream &is, stn_t &indv)
-{
-	is >> indv.name >>  indv.score;
+istream& read(istream &is, stn_t &indv) {
+	is >> indv.name;
 	return is;
 }
 
-bool result_sorter(stn_t a, stn_t b)
-{
-	if(a.score > b.score)
-		return true;
-	else
-		return false;
-	if(a.name < b.name)
+/*Sorter function*/
+bool result_sorter(stn_t a, stn_t b) {
+	if (a.score > b.score)
 		return true;
 	else
 		return false;
 }
+
 /*First question*/
 int quiz1(int score)
 {
 	float ans1;
-	cout<< "First Question: What is the decimal number of this floating point number: \n";
-	cout<< " (+)0.1101101x2^4 " << endl;
+	cout << "First Question: What is the decimal number of this floating point number: \n";
+	cout << " (+)0.1101101x2^4 " << endl;
 	cin >> ans1;
 	if (ans1 != 13.625)
 	{
@@ -50,10 +45,10 @@ int quiz1(int score)
 		return score;
 	}
 	else
-		{
+	{
 		cout << "Correct" << endl;
 		score += 5;
-		}
+	}
 	return score;
 }
 
@@ -61,7 +56,7 @@ int quiz1(int score)
 int quiz2(int score)
 {
 	bool ans2;
-	cout<<"Second Question: Is dr. Takis Zourntos a strict Professor? Type: 1= yes, 0 = no "<< endl;
+	cout << "Second Question: Is dr. Takis Zourntos a strict Professor? Type: 1= yes, 0 = no "<< endl;
 	cin >> ans2;
 	if (ans2)
 	{
@@ -72,77 +67,73 @@ int quiz2(int score)
 		throw logic_error("Wrong answer");
 	return score;
 }
-/*Third Question*/
 
+/*Third Question*/
 int quiz3(int score)
 {
 	int ans3;
-	cout << "Third question: 11^2 = ? :" <<endl;
+	cout << "Third question: 11^2 = ? :" << endl;
 	cin >> ans3;
 	if (ans3 == 121)
 	{
 		cout << "Correct! \n";
-		score +=5;
+		score += 5;
 	}
 	else
 		throw logic_error("Wrong answer");
 	return score;
 }
 
+/*Test function*/
 stn_t test(stn_t a)
 {
 	int score = 0;
 	try {
 		score = quiz1(score);
-	}
-	catch (logic_error str)
-	{
+	} catch (logic_error str) {
 		cout << str.what() << endl;
 	}
 	cout << "Your score: " << score << endl;
 	try {
 		score = quiz2(score);
-	}
-	catch (logic_error str1)
-	{
+	} catch (logic_error str1) {
 		cout << str1.what() << endl;
 	}
 	cout << "Your score: " << score << endl;
 	try {
 		score = quiz3(score);
+	} catch (logic_error str2) {
+		cout << str2.what() << endl;
 	}
-	catch (logic_error str1)
-	{
-		cout << str1.what() << endl;
-	}
-
-	cout << "Your score: " << score << endl;
+	/*Displaying the score and return score of the individual*/
+	cout << "Your score: " << score << endl << endl;
 	a.score = score;
 	return a;
 }
 
+/*Main Function*/
 int main()
 {
-	/*Variables declearation*/
+	/*Variables declaration*/
 	vector<stn_t> victims;
 	stn_t indv;
 	cout << "Enter your name and take the test" << endl;
-	cout << "Press Ctrl + D when done" << endl;
-	while (cin >> indv.name)
+	cout << "Press Ctrl + D when done" << endl << endl;
+	/*Looping and taking exam for each student*/
+	while (read(cin, indv))
 	{
 		indv = test(indv);
 		victims.push_back(indv);
-		cout << "Student Name: "<<indv.name << "--- Score: " <<indv.score << endl;
+		cout << "Student Name: " << indv.name << "--- Score: " << indv.score
+				<< endl << endl;
 	}
-	/*
-	 * Sorting and Displaying the result.
-	 * */
-	sort(victims.begin(),victims.end(),result_sorter);
+	/*Sorting and Displaying the result.*/
+	sort(victims.begin(), victims.end(), result_sorter);
 	cout << " The Results are: " << endl;
-	for(vector<stn_t>::size_type ii = 0; ii !=victims.size(); ++ii)
-		{
-		cout <<"Student: " << victims[ii].name <<"---Score: " << victims[ii].score << endl;
-		}
-
+	for (vector<stn_t>::size_type ii = 0; ii != victims.size(); ++ii)
+	{
+		cout << "Student: " << victims[ii].name << "---Score: "
+				<< victims[ii].score << endl;
+	}
 	return 0;
 }
